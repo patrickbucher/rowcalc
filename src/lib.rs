57ef51278e60002f73e5442674bd2e662af71320
Patrick Bucher <patrick.bucher@mailbox.org>
2024-09-04 20:10:37 +0200

    compute and output phases

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,37 @@
+use std::fmt::{self, Display, Formatter};
+use std::time::Duration;
+
+pub enum Phase {
+    Rowing { dist: u32, time: Duration },
+    Resting { time: Duration },
+}
+
+impl Display for Phase {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match self {
+            Phase::Rowing { dist, time } => {
+                write!(f, "row {}m until {}", dist, fmt_mins_secs(time))
+            }
+            Phase::Resting { time } => write!(f, "break until {}", fmt_mins_secs(time)),
+        }
+    }
+}
+
+fn fmt_mins_secs(time: &Duration) -> String {
+    let (mins, secs) = secs_to_mins_secs(time);
+    if mins > 0 {
+        format!("{mins}m{secs}s")
+    } else {
+        format!("{secs}s")
+    }
+}
+
+fn secs_to_mins_secs(time: &Duration) -> (u64, u64) {
+    let secs = time.as_secs();
+    let mins = secs / 60;
+    let secs = secs % 60;
+    (mins, secs)
+}
 pub fn calc_split_time(dist: u32, secs: u32, size: f32) -> (u32, f32) {
     let n_500_splits = dist as f32 / size;
     let split_time_secs = secs as f32 / n_500_splits;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 use duration_str::parse;
-use rowcalc::calc_split_time;
+use rowcalc::{calc_split_time, Phase};
 use std::time::Duration;
 
 #[derive(Parser, Debug)]
@@ -24,11 +24,6 @@ struct Args {
     breaks: Option<u32>,
 }
 
-enum Phase {
-    Rowing { dist: u32, time: Duration },
-    Resting { time: Duration },
-}
-
 fn main() {
     let args = Args::parse();
     let dist = args.dist;
@@ -48,11 +43,32 @@ fn main() {
     println!("total rowing time: {total_rowing_time:?}");
 
     let stint_rowing_time = total_rowing_time.div_f32(stints as f32);
-    println!("sting rowing time: {stint_rowing_time:?}");
+    println!("stint rowing time: {stint_rowing_time:?}");
 
     let velocity = dist as f32 / total_rowing_time.as_secs_f32(); // m/s
     println!("velocity: {velocity:?}m/s");
 
-    let split_time = calc_split_time(dist, time.as_secs() as u32, 500 as f32);
+    let split_time = calc_split_time(dist, time.as_secs() as u32, 500_f32);
     println!("500m split time: {}m{}s", split_time.0, split_time.1);
+
+    println!();
+
+    let mut phases: Vec<Phase> = Vec::new();
+    let mut running_time = Duration::new(0, 0);
+    for i in 0..(stints + breaks) {
+        let phase = if i % 2 == 0 {
+            running_time = running_time.saturating_add(stint_rowing_time);
+            Phase::Rowing {
+                dist: dist / stints,
+                time: running_time,
+            }
+        } else {
+            running_time = running_time.saturating_add(pause);
+            Phase::Resting { time: running_time }
+        };
+        phases.push(phase);
+    }
+    for phase in phases {
+        println!("{}", phase);
+    }
 }
