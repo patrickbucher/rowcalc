cb87d0da822552d578cd8e587a9a82f4f10b25bd
Patrick Bucher <patrick.bucher@mailbox.org>
2024-09-07 22:41:49 +0200

    cosmetic changes

--- src/lib.rs
+++ src/lib.rs
@@ -29,28 +29,25 @@ pub fn calc_phases(
     let mut elapsed_dist: f32 = 0.0;
     let mut elapsed_time = Duration::new(0, 0);
     let mut breaks_taken: usize = 0;
-    while elapsed_dist < dist as f32 {
-        let dist_left = dist as f32 - elapsed_dist;
+    while elapsed_dist < dist {
+        let dist_left = dist - elapsed_dist;
         let time_left = time.saturating_sub(elapsed_time);
-        let (split_dist, split_time) = if dist_left < split_dist as f32 {
+        let (split_dist, split_time) = if dist_left < split_dist {
             (dist_left, time_left)
         } else {
             (split_dist, split_time)
         };
-
         elapsed_dist += split_dist;
         elapsed_time = elapsed_time.saturating_add(split_time);
-
         phases.push(Phase::Rowing {
             time: elapsed_time,
             dist: elapsed_dist as u32,
         });
         let stints_finished = ((elapsed_dist / dist) * stints as f32).floor() as usize;
         if stints_finished == stints {
-            // TODO: is this still needed?
             break;
         }
-        if stints_finished as usize > breaks_taken {
+        if stints_finished > breaks_taken {
             elapsed_time = elapsed_time.saturating_add(pause);
             phases.push(Phase::Resting { time: elapsed_time });
             breaks_taken += 1;
@@ -68,18 +65,19 @@ fn fmt_mins_secs(time: &Duration) -> String {
     }
 }
 
-fn secs_to_mins_secs(time: &Duration) -> (u64, u64) {
-    let secs = time.as_secs();
-    let mins = secs / 60;
-    let secs = secs % 60;
-    (mins, secs)
-}
 pub fn calc_split_time(dist: u32, secs: u32, size: f32) -> (u32, f32) {
     let n_500_splits = dist as f32 / size;
     let split_time_secs = secs as f32 / n_500_splits;
     let split_mins = split_time_secs as u32 / 60;
     let split_secs = split_time_secs - (split_mins * 60) as f32;
     (split_mins, round(split_secs, 1.0))
+}
+
+fn secs_to_mins_secs(time: &Duration) -> (u64, u64) {
+    let secs = time.as_secs();
+    let mins = secs / 60;
+    let secs = secs % 60;
+    (mins, secs)
 }
 
 fn round(value: f32, granularity: f32) -> f32 {
--- src/main.rs
+++ src/main.rs
@@ -22,47 +22,40 @@ struct Args {
     /// Breaks as number (e.g. 3)
     #[arg(short, long)]
     breaks: Option<u32>,
-    // TODO: specify split time (e.g. 5m) or split distance (e.g. 1000m)
 }
 
 fn main() {
     let args = Args::parse();
     let dist = args.dist;
-    let time = parse(args.time).expect("time indication");
+    let time = parse(&args.time).expect("time indication");
     let pause = parse(args.pause.unwrap_or("0s".into())).unwrap_or(Duration::new(0, 0));
     let breaks = args.breaks.unwrap_or(0);
     let stints = (breaks + 1) as usize;
-    println!(
-        "row {}m in {:?} with {} breaks of {:?}",
-        dist, time, breaks, pause
-    );
 
     let total_break_time = pause.saturating_mul(breaks);
-    println!("total pause time: {total_break_time:?}");
-
     let total_rowing_time = time.saturating_sub(total_break_time);
-    println!("total rowing time: {total_rowing_time:?}");
-
-    let stint_rowing_time = total_rowing_time.div_f32(stints as f32);
-    println!("stint rowing time: {stint_rowing_time:?}");
-
-    let velocity = dist as f32 / total_rowing_time.as_secs_f32(); // m/s
-    println!("velocity: {velocity:?}m/s");
-
     let split_time = calc_split_time(dist, time.as_secs() as u32, 500_f32);
-    println!("500m split time: {}m{}s", split_time.0, split_time.1);
+    let split_time_msg = format!("{}m{}s", split_time.0, split_time.1);
+    let velocity = dist as f32 / total_rowing_time.as_secs_f32();
+
+    let break_msg = if breaks > 0 {
+        format!("with {breaks} breaks of {pause:?}")
+    } else {
+        String::from("without any breaks")
+    };
+    println!(
+        "To row {}m in {} {} a 500m split time of {} and a velocity of {:.2}m/s is needed.",
+        dist, args.time, break_msg, split_time_msg, velocity
+    );
 
     let stint_dist: f32 = dist as f32 / stints as f32;
     let stint_time: f32 = total_rowing_time.as_secs_f32() / stints as f32;
 
-    let split_dist: f32 = 500_f32; // TODO: fill with argument (to be defined) from above
+    let split_dist: f32 = 500_f32;
     let split_time = Duration::from_secs((stint_time / (stint_dist / split_dist)).round() as u64);
-    println!("split dist: {}m", split_dist);
-    println!("split time: {:?}", split_time);
 
-    println!();
-
-    for phase in calc_phases(dist as f32, split_dist, time, split_time, stints, pause) {
-        println!("{}", phase);
+    let phases = calc_phases(dist as f32, split_dist, time, split_time, stints, pause);
+    for (i, phase) in phases.iter().enumerate() {
+        println!("{i:2}) {}", phase);
     }
 }
